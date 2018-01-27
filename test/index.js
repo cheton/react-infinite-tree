08b86b9cfe93c8106c4e034fcc435babb7985c72
Cheton Wu <cheton@gmail.com>
2018-01-28 00:09:29 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import InfiniteTree from '../src';
+import Tree from '../examples/Tree';
 
-test('noop', (t) => {
+test('<InfiniteTree />', (t) => {
+    const onUpdate = (node) => {
+        t.equal(node.id, '<root>.0');
+    };
+
+    const wrapper = mount((
+        <Tree onUpdate={onUpdate} />
+    ));
+
+    t.equal(wrapper.find(InfiniteTree).length, 1, 'should render <InfiniteTree /> component');
     t.end();
 });
